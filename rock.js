d0333f7d54c3facdb639cc821d4c60918fd93613
CatboyWizard <90338417+CatboyWizard@users.noreply.github.com>
2022-02-15 13:23:13 -0500

    Add files via upload

--- rock.js
+++ rock.js
@@ -9,9 +9,9 @@ const game = () => {
  
     // Function to
     const playGame = () => {
-        const rockBtn = document.querySelector('.rock');
-        const paperBtn = document.querySelector('.paper');
-        const scissorBtn = document.querySelector('.scissor');
+        const rockBtn = document.getElementById('rock');
+        const paperBtn = document.getElementById('paper');
+        const scissorBtn = document.getElementById('scissor');
         const playerOptions = [rockBtn,paperBtn,scissorBtn];
         const computerOptions = ['rock','paper','scissors']
          
@@ -40,17 +40,16 @@ const game = () => {
     }
 
 // Function to decide winner
-    const winner = (player,computer) => {
+const winner = (player,computer) => {
     const result = document.querySelector('.result');
     const playerScoreBoard = document.querySelector('.p-count');
     const computerScoreBoard = document.querySelector('.c-count');
     
     player = player.toLowerCase();
+    // player = player.toLowerCase();
     computer = computer.toLowerCase();
-    }
-}
     
-if(player === computer){
+    if(player === computer){
         result.textContent = 'Tie'
     }
     else if(player == 'rock'){
@@ -86,51 +85,45 @@ if(player === computer){
                 playerScoreBoard.textContent = playerScore;
             }
         }
-        else if(player == 'paper')
-            if(computer == 'bomb'){
-                result.textContent = 'Computer Won';
-                computerScore++;
-                computerScoreBoard.textContent = computerScore;
-            }else{
-                result.textContent = 'Player Won';
-                playerScore++;
-                playerScoreBoard.textContent = playerScore;
-            }
- 
-    // Function to run when game is over
-    const gameOver = (playerOptions,movesLeft) => {
- 
-        const chooseMove = document.querySelector('.move');
-        const result = document.querySelector('.result');
-        const reloadBtn = document.querySelector('.reload');
- 
-        playerOptions.forEach(option => {
-            option.style.display = 'none';
-        })
- 
-      
-        chooseMove.innerText = 'Game Over!!'
-        movesLeft.style.display = 'none';
- 
-        if(playerScore > computerScore){
-            result.style.fontSize = '2rem';
-            result.innerText = 'You Lost The Game';
-            result.style.color = 'red';
-        }
-        else{
-            result.style.fontSize = '2rem';
-            result.innerText = 'Tie';
-            result.style.color = 'grey'
-        }
-        reloadBtn.innerText = 'Restart';
-        reloadBtn.style.display = 'flex'
-        reloadBtn.addEventListener('click',() => {
-            window.location.reload();
-        })
-    } 
- 
-// Calling playGame function inside game
-    playGame();
- 
+}
+
+// Function to run when game is over
+const gameOver = (playerOptions,movesLeft) => {
+
+    const chooseMove = document.querySelector('.move');
+    const result = document.querySelector('.result');
+    const reloadBtn = document.querySelector('.reload');
+
+    playerOptions.forEach(option => {
+        option.style.display = 'none';
+    })
+
+  
+    chooseMove.innerText = 'Game Over!!'
+    movesLeft.style.display = 'none';
+
+    if(playerScore > computerScore){
+        result.style.fontSize = '2rem';
+        result.innerText = 'You Lost The Game';
+        result.style.color = 'red';
+    }
+    else{
+        result.style.fontSize = '2rem';
+        result.innerText = 'Tie';
+        result.style.color = 'grey'
+    }
+    reloadBtn.innerText = 'Restart';
+    reloadBtn.style.display = 'flex'
+    reloadBtn.addEventListener('click',() => {
+        window.location.reload();
+    })
+}
+
+// Calling playGame function inside game 
+playGame();
+
 // Calling the game function
+
+}
+
 game();
